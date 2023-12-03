d2de99d6a62e9fdb47386e81d1e3108308d4e20b
Toan <phanqu6ctoan@gmail.com>
2023-12-03 23:49:31 +0700

    chuyen vao trang admin khi login

--- src/main/java/com/tdtu/backend/config/SecurityConfig.java
+++ src/main/java/com/tdtu/backend/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
-                        .defaultSuccessUrl("/", true)
+                        .successHandler(new CustomLoginSuccessHandler())
                 )
                 .logout(logout -> logout
                         .permitAll()
